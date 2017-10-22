a6402b189897860db463b39da8aa31be9091a4a6
Ed Daniels <ed.daniels@foonlysystems.com>
2017-10-22 12:44:43 -0400

    Initial Commit

---
+++ gitrdone2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitrdone2
+//
+//  Created by Ed Daniels on 10/22/17.
+//  Copyright © 2017 Foonly Systems LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
